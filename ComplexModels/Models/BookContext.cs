98a7afa1814032d559e85aaf3152cd9f844a8062
alex.paclin <alex.paclin@gmail.com>
2016-12-05 20:59:27 +0500

    Миграция баз данных

--- ComplexModels/Models/BookContext.cs
+++ ComplexModels/Models/BookContext.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace ComplexModels.Models
 {
-    public class BookContext : DbContext
+	public class BookContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
-    }
+		public DbSet<User> Users { get; set; }
+		public DbSet<Company> Companies { get; set; }
+	}
 }
--- ComplexModels/Models/BookDbInitializer.cs
+++ ComplexModels/Models/BookDbInitializer.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace ComplexModels.Models
 {
-    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+	public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
     {
         protected override void Seed(BookContext db)
         {
             db.Books.Add(new Book { Id = 1, Name = "Война и мир", AuthorId = 2 });
             db.Books.Add(new Book { Id = 2, Name = "Отцы и дети", AuthorId = 1 });
             db.Books.Add(new Book { Id = 3, Name = "Чайка", AuthorId = 3 });
-
-
+			
             db.Authors.Add(new Author { Id = 1, Name = "Тургенев И.С." });
             db.Authors.Add(new Author { Id = 2, Name = "Толстой Л.Н." });
             db.Authors.Add(new Author { Id = 3, Name = "Чехов А.П." });
 
-            base.Seed(db);
+			db.Users.Add(new User { Id = 1, Name = "John", Age = 40 });
+			db.Users.Add(new User { Id = 2, Name = "Paul", Age = 74 });
+			db.Users.Add(new User { Id = 3, Name = "George", Age = 58 });
+			db.Users.Add(new User { Id = 3, Name = "Ringo", Age = 76 });
+
+			db.Companies.Add(new Company { Id = 1, Name = "Рога и Копыта" });
+
+			base.Seed(db);
         }
     }
 }
